a57a05af14ef51e8fc58be0d212667f61890c956
Nuoscaris <nuoscarisivira@gmail.com>
2021-05-27 19:47:31 -0500

    ejercicio

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './App.css';
+import Layouts from './components/Layouts';
+
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Layouts/>
+    </div>
+  );
+}
+
+export default App;
